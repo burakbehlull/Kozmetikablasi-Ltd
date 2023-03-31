8bc1f79164c2e9fdfe1552a92abf14ffa1475899
31000s <burakyabgu@gmail.com>
2023-03-31 23:21:51 +0300

    feat: Installing and using the cors package

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
+@cross_origin()
 def index():
     return "<h1>Index</h1>"
 
